bb722581940e269845745e54a9de0798ceffb244
Олег Фузеев <fuzeev.o@perfluence.io>
2025-04-02 00:58:54 +0300

    jwt

--- public/index.php
+++ public/index.php
@@ -7,5 +7,7 @@ use App\Kernel;
 require_once dirname(__DIR__) . '/vendor/autoload_runtime.php';
 
 return function (array $context) {
+    error_reporting(E_ALL & ~E_DEPRECATED); //в пакете с refresh токенами есть deprecated код с php 8.4
+
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
